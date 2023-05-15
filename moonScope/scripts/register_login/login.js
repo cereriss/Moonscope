ca620a338862d95e09be9a8130d32bb247ea01a3
Michelle Aprile <michellebellissima56@gmail.com>
2023-05-15 10:40:49 +0200

    continuo registrati front-end

--- moonScope/scripts/register_login/login.js
+++ moonScope/scripts/register_login/login.js
@@ -1,1 +1,9 @@
-const signupbtn = document.querySelector(".signupbtn");
+const signupbtn = document.querySelector(".btn-signup");
+
+function login() {}
+
+function signup() {
+  window.location.href = "../pages/register.html";
+}
+
+signupbtn.addEventListener("click", signup);
